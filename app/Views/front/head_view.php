bbb6ab601203ff9712d23dc5b0dbc57d2f035c92
daiazabala <daianazabala1303@gmail.com>
2023-10-05 17:50:38 -0300

    Proyecto tramo 3

---
+++ app/Views/front/head_view.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <title><?php echo($titulo);?></title>
+     
+    <!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css"> -->
+
+    <!--se utiliza para cargar un estilo (css)-->
+    <link rel="stylesheet" href="assets/css/styless.css"> 
+    <!--se utiliza para cargar un estilo (css), pero en este caso se utiliza la hoja de estilo de la biblioteca Bootstrap, que se carga desde un servidor de contenido estático (CDN)-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+    <!-- El ruteo al servidor propio no lo utilizo ya que estoy usando el SDN de la pagina bootstrap -->
+
+</head>
+
+<header class="p-4" style="background-color: #20a951;">
+    <h1 class="text-white text-center">Bienvenido a nuestra pagina principal</h1>
+</header>
+
+<body>
